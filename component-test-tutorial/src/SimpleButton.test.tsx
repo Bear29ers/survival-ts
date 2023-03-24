730d8432be6e2cde807be6bf3dd3ca3dcf0cd4c4
Yuta Okuma <y.dream.nest@icloud.com>
2023-03-24 18:22:25 +0900

    feat: :sparkles: testing-libraryを使ったコンポーネントテスト

---
+++ component-test-tutorial/src/SimpleButton.test.tsx
@@ -0,0 +1,24 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import { render, screen, waitFor } from "@testing-library/react";
+import SimpleButton from "./SimpleButton";
+import "@testing-library/jest-dom";
+import userEvent from "@testing-library/user-event";
+
+test("ボタンをクリックするとON/OFFの表示が切り替わる", async () => {
+  // SimpleButtonのレンダリング
+  render(<SimpleButton />);
+  // ボタン要素を取得
+  const simpleButton = screen.getByRole("button");
+  // テキストがOFFになっていることを確認
+  expect(simpleButton).toHaveTextContent("OFF");
+  // クリックイベントでボタンをクリック
+  userEvent.click(simpleButton);
+
+  await waitFor(() => {
+    // テキストがONになっていることを確認
+    expect(simpleButton).toHaveTextContent("ON");
+  });
+});
